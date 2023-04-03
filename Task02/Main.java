e2b02a66efae9a7d2e0d0cce0534df00d9427f25
Ільницький Максим <112692170+IlnitskijMaksim@users.noreply.github.com>
2023-04-03 19:37:01 +0300

    Update Main.java

--- Task02/Main.java
+++ Task02/Main.java
@@ -1,7 +1,15 @@
 public class Main {
 
-    public static void main(String[] args) {
-        double alpha = 30; // значення кута у гардусах
+
+/** 
+ *
+ * Main
+ *
+ * @param args  the args
+ */
+    public static void main(String[] args) { 
+
+        double alpha = 45; // значення кута у гардусах
         int sequence_length =  CalculateObject.calc(alpha);
         System.out.println("Max units length equal " + sequence_length);
         
