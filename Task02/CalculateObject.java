8548660ced096cfc555b20e9970e97cf5d2030b7
Ільницький Максим <112692170+IlnitskijMaksim@users.noreply.github.com>
2023-04-03 13:46:32 +0300

    Create CalculateObject.java

---
+++ Task02/CalculateObject.java
@@ -0,0 +1,25 @@
+public class CalculateObject {
+    public static int getSequenceLength(String binaryString) {
+        int maxLength = 0;
+        int currentLength = 0;
+        for (int i = 0; i < binaryString.length(); i++){
+            if (binaryString.charAt(i) == '1'){
+            currentLength++;
+            maxLength = Math.max(maxLength, currentLength);
+        } else {
+                currentLength = 0;
+                }
+    }
+        return maxLength;
+    }
+    
+    public static int calc(double alpha){
+        double radians = alpha * Math.PI / 180; //переведення кута у радіани
+        double cos_alpha = Math.cos(radians); //знаходимо значення косинуса кута
+        double sum = Math.pow(10 * cos_alpha, 2) + Math.pow(10 * cos_alpha, 3); //обчислення суми квадрата і куба
+        int sum_rounded = (int) Math.round(sum); //округлення до цілого числа 
+        String sum_binary = Integer.toBinaryString(sum_rounded); //переведення у двійкову систему 
+        int max_sequence_length = getSequenceLength(sum_binary); //знаходження найбільшої довжини послідовності одиниць
+        return max_sequence_length;
+    }
+}
