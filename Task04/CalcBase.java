864ea5ead573432a16329e7da56616344373ad1c
Ільницький Максим <112692170+IlnitskijMaksim@users.noreply.github.com>
2023-04-05 16:56:30 +0300

    Create CalcBase.java

---
+++ Task04/CalcBase.java
@@ -0,0 +1,9 @@
+public class CalcBase {
+    public double calculate(double n) {
+        return n * 2;
+    }
+    
+    public double calculate(double n, double m) {
+        return n * m;
+    }
+}
