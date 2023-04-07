650eda1956b702d198f38c68bd9923b92dbf61a8
Ільницький Максим <112692170+IlnitskijMaksim@users.noreply.github.com>
2023-04-07 13:19:55 +0300

    Update ParallelProcessing.java

--- Task06/ParallelProcessing.java
+++ Task06/ParallelProcessing.java
@@ -1,3 +1,8 @@
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -5,50 +10,127 @@ import java.util.Random;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.SwingUtilities;
+
 
  /**
- * The class Main
+ * The class Parallel processing extends J frame implements action listener
  */ 
-public class Main {
+public class ParallelProcessing extends JFrame implements ActionListener {
 
+    private static final long serialVersionUID = 1L;
+    
+    private JButton startButton;
+    private JTextArea outputArea;
+    
 
 /** 
  *
- * Main
+ * Parallel processing
  *
- * @param args  the args. 
- * @throws   InterruptedException 
+ * @return public
  */
-    public static void main(String[] args) throws InterruptedException { 
+    public ParallelProcessing() { 
 
+        setTitle("Parallel Processing Demo");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        startButton = new JButton("Start");
+        startButton.addActionListener(this);
+        JPanel buttonPanel = new JPanel(new FlowLayout());
+        buttonPanel.add(startButton);
+        
+        outputArea = new JTextArea();
+        outputArea.setEditable(false);
+        JScrollPane scrollPane = new JScrollPane(outputArea);
+        scrollPane.setPreferredSize(new Dimension(400, 300));
+        
+        getContentPane().add(buttonPanel, BorderLayout.NORTH);
+        getContentPane().add(scrollPane, BorderLayout.CENTER);
+        
+        pack();
+        setLocationRelativeTo(null);
+    }
+    
+    @Override
+
+/** 
+ *
+ * Action performed
+ *
+ * @param e  the e. 
+ */
+    public void actionPerformed(ActionEvent e) { 
+
+        if (e.getSource() == startButton) {
+            startParallelProcessing();
+        }
+    }
+    
+
+/** 
+ *
+ * Start parallel processing
+ *
+ */
+    private void startParallelProcessing() { 
 
         // Створюємо колекцію з випадковими числами
         List<Integer> numbers = new ArrayList<>();
         Random random = new Random();
-        for (int i = 0; i < 100000; i++) {
-            numbers.add(random.nextInt(10000));
+        for (int i = 0; i < 100; i++) {
+            numbers.add(random.nextInt(100));
         }
 
         // Створюємо чергу команд
-        BlockingQueue<Runnable> queue = new ArrayBlockingQueue<>(100);
+        BlockingQueue<Runnable> queue = new ArrayBlockingQueue<>(10);
         
         // Додаємо команди в чергу
-        queue.put(() -> System.out.println("Min: " + Collections.min(numbers)));
-        queue.put(() -> System.out.println("Max: " + Collections.max(numbers)));
-        queue.put(() -> System.out.println("Avg: " + numbers.stream().mapToInt(Integer::intValue).average().getAsDouble()));
-        queue.put(() -> {
+        queue.add(() -> output("Min value: " + Collections.min(numbers)));
+        queue.add(() -> output("Max value: " + Collections.max(numbers)));
+        queue.add(() -> output("Avg value: " + numbers.stream().mapToInt(Integer::intValue).average().getAsDouble()));
+        queue.add(() -> {
             List<Integer> filtered = new ArrayList<>();
             for (int num : numbers) {
                 if (num % 2 == 0) {
                     filtered.add(num);
                 }
             }
-            System.out.println("Filtered: " + filtered);
+            output("Filtered: " + filtered);
         });
 
         // Створюємо п'ять робочих потоків
         for (int i = 0; i < 5; i++) {
             new WorkerThread(queue).start();
         }
+    }
+    
+
+/** 
+ *
+ * Output
+ *
+ * @param message  the message. 
+ */
+    private void output(String message) { 
+
+        SwingUtilities.invokeLater(() -> outputArea.append(message + "\n"));
+    }
+    
+
+/** 
+ *
+ * Main
+ *
+ * @param args  the args. 
+ */
+    public static void main(String[] args) { 
+
+        SwingUtilities.invokeLater(() -> new ParallelProcessing().setVisible(true));
     }
 }
